564b4be9e997f47b3599f646c41d7e08c21d9256
CMountford <christopher.mountford@abagri.com>
2016-11-14 14:53:11 +0000

    Basic layout added

--- GUI.py
+++ GUI.py
@@ -14,28 +14,32 @@ class PdfGUI:
 
         # Quit app button
         self.quit_button = Button(master, text='Close Window', command=master.quit)
-        self.quit_button.pack()
 
         # File 1 label
         self.first_file_label = Label(master, text='Select a file')
-        self.first_file_label.pack()
 
         # File 1 browse button - Command argument as a lambda function so does not run upon startup
         self.first_file_browser = Button(master, text='Browse...', command=lambda: self.file_browser())
-        self.first_file_browser.pack()
 
         # File 2 label
         self.second_file_label = Label(master, text='Select a file')
-        self.second_file_label.pack()
 
         # File 2 browse button
         self.second_file_browser = Button(master, text='Browse...', command=lambda: self.file_browser())
-        self.second_file_browser.pack()
+
+        # Layout
+
+        self.first_file_label.grid(row=0, column=0)
+        self.first_file_browser.grid(row=0, column=1)
+        self.second_file_label.grid(row=1, column=0)
+        self.second_file_browser.grid(row=1, column=1)
+        self.quit_button.grid(row=10, column=1)
 
     # Function to open file browser
     def file_browser(self):
         self.file_name = filedialog.askopenfilename()
 
+    # def create_button(self):
 
 if __name__ == '__main__':
     root = Tk()
