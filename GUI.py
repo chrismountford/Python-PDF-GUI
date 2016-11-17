e4f2b1ef1981a8b414a3191e9abf7d62edd502b5
CMountford <christopher.mountford@abagri.com>
2016-11-17 11:13:19 +0000

    Some formatting done. Directory to save functionality added to a button. Name ectry still needs to be easier

--- GUI.py
+++ GUI.py
@@ -1,7 +1,5 @@
 # TODO: Layout of app
 # TODO: Dynamic UI to allow for multiple merges
-# TODO: Build in PDF functionality
-# TODO: Turn labels into entry boxes
 
 from tkinter import *
 from tkinter import filedialog
@@ -12,53 +10,72 @@ import PDFMethods
 class PdfGUI:
     def __init__(self, master):
         self.master = master
-        master.title = 'PDF Merger'
-        master.geometry("400x300")
+        master.title('PDF Merger')
+        master.geometry("300x300")
+
+        bg_col = '#FFFFFF'
+        font_style = 'Helvetica'
+
+
+
+        master.configure(background=bg_col)
 
         self.file_name_text = StringVar()
         self.file_name2_text = StringVar()
+        self.save_dir_text = StringVar()
 
         # Quit app button
         self.quit_button = Button(master, text='Close Window', command=master.quit)
 
         # File 1
-        self.first_file_label = Label(master, text='Select a file')
-
+        self.first_file_label = Label(master, text='Select a file:', bg=bg_col)
         # Command argument as a lambda function so does not run upon startup
-        self.first_file_browser = Button(master, text='Browse...', command=lambda: self.file_browser())
-
+        self.first_file_browser = Button(master, text='Browse', command=lambda: self.file_browser())
         self.first_select_label = Entry(master, textvariable=self.file_name_text)
 
         # File 2
-        self.second_file_label = Label(master, text='Select a file')
-
-        self.second_file_browser = Button(master, text='Browse...', command=lambda: self.file_browser2())
-
+        self.second_file_label = Label(master, text='Select a file:', bg=bg_col)
+        self.second_file_browser = Button(master, text='Browse', command=lambda: self.file_browser2())
         self.second_select_label = Entry(master, textvariable=self.file_name2_text)
 
         # Output naming
-        self.merged_file_name = Entry(master)
+        self.merged_file_name = Entry(master, textvariable=self.save_dir_text)
+        self.merged_file_label = Label(master, text='Select a directory:', bg=bg_col)
+        self.merged_directory_browser = Button(master, text='Browse', command=lambda: self.save_dir())
 
         # Merge button
-
         self.merge_button = Button(master, text='Merge',
                                    command=lambda:
                                    PDFMethods.combine_pdf(self.file_name_text.get(), self.file_name2_text.get(),
                                                           self.merged_file_name.get()))
 
+        # Title
+        self.title_label = Label(master, text='PDF Merger', font=(font_style, 16), bg=bg_col)
+
+        # Blank label
+        self.blank_label = Label(master, bg=bg_col)
+
         # LAYOUT #
 
-        self.first_file_label.grid(row=0, column=0)
-        self.first_file_browser.grid(row=0, column=1)
-        self.first_select_label.grid(row=0, column=2)
+        self.title_label.grid(row=0, column=1)
 
-        self.second_file_label.grid(row=1, column=0)
-        self.second_file_browser.grid(row=1, column=1)
-        self.second_select_label.grid(row=1, column=2)
+        self.blank_label.grid(row=1, column=1)
 
-        self.merged_file_name.grid(row=2, column=0)
+        self.first_file_label.grid(row=2, column=0)
+        self.first_select_label.grid(row=2, column=1)
+        self.first_file_browser.grid(row=2, column=2)
 
-        self.merge_button.grid(row=3, column=0)
+        self.second_file_label.grid(row=3, column=0)
+        self.second_select_label.grid(row=3, column=1)
+        self.second_file_browser.grid(row=3, column=2)
+
+        self.blank_label.grid(row=4, column=0)
+
+        self.merged_file_label.grid(row=5, column=0)
+        self.merged_file_name.grid(row=5, column=1)
+        self.merged_directory_browser.grid(row=5, column=2)
+
+        self.merge_button.grid(row=6, column=0)
 
         self.quit_button.grid(row=10, column=1)
 
@@ -70,6 +87,10 @@ class PdfGUI:
     def file_browser2(self):
         file_name2 = filedialog.askopenfilename()
         self.file_name2_text.set(file_name2)
+
+    def save_dir(self):
+        save_dir_text = filedialog.askdirectory()
+        self.save_dir_text.set(save_dir_text)
 
 if __name__ == '__main__':
     root = Tk()
